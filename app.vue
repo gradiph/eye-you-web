6a322616954c364f791b5e1e5abe577530d5e99e
Gradiyanto Putera Husein <gradiph@gmail.com>
2024-05-10 08:32:40 +0700

    add Vercel Speed Insights

--- app.vue
+++ app.vue
@@ -3,9 +3,12 @@
     <NuxtLayout :name="layout">
       <NuxtPage />
     </NuxtLayout>
+
+    <SpeedInsights />
   </div>
 </template>
 
 <script setup>
+import { SpeedInsights } from '@vercel/speed-insights/nuxt';
 const layout = computed(() => useToken().value === undefined ? 'default' : 'logged-in')
 </script>
